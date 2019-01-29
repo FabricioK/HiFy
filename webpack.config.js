05ec1f27b25976acdde532caf4feba162bd87aeb
Fabricio dos santos antunes <fabriciodossa@hotmail.com>
2019-01-29 09:40:02 -0200

    ignora .env no servidor

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,11 @@ const dotenv = require('dotenv');
 module.exports = () => {
     const env = dotenv.config().parsed;
     // reduce it to a nice object, the same as before
-    const envKeys = Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next]);
-        return prev;
-    }, {});
+    if (env)
+        const envKeys = Object.keys(env).reduce((prev, next) => {
+            prev[`process.env.${next}`] = JSON.stringify(env[next]);
+            return prev;
+        }, {});
 
     return {
         entry: __dirname + "/src/index.jsx",
